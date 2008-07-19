7fd6db44cbf765ce623fc1da33c24d6a98d6367a
Bryan Ray <bryansray@gmail.com>
2008-07-19 09:52:12 -0500

    Added a main controller with an index page

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,6 +1,7 @@
 class Main < Application
   
   def index
+    @posts = Post.all :limit => 5
     render
   end
   
--- spec/controllers/main_spec.rb
+++ spec/controllers/main_spec.rb
@@ -2,6 +2,20 @@ require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
 
 describe Main, "index action" do
   before(:each) do
-    dispatch_to(Main, :index)
+    @post = mock(:post)
+    @posts = [@post]
   end
+
+  def do_get
+    dispatch_to(Main, :index) do |controller|
+      controller.stub!(:render)
+      controller.should_receive(:render)
+    end
+  end
+
+  it "should find a list of posts" do
+    Post.should_receive(:all).and_return(@posts)
+    do_get
+  end
+
 end
