4aa22f8d56ebc7a1c6b1a340f19620e54338f027
Bryan Ray <bryansray@gmail.com>
2008-07-19 19:34:46 -0500

    Adding RedCloth as a dependency

--- config/init.rb
+++ config/init.rb
@@ -59,6 +59,7 @@ Gem.path.unshift(Merb.root / "gems")
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
 
+dependency "RedCloth"
 dependency "merb-assets"
 dependency "merb-haml"
 dependency "merb-mailer"
