35cd793de239ac427666a39082ad80a4ccca3438
Bryan Ray <bryansray@gmail.com>
2008-07-20 19:34:09 -0500

    Cleaning up the deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,13 @@
-
 default_run_options[:pty] = true
 set :scm, :git
 set :scm_username,  'bryansray'
 set :application, "smerb"
 set :repository,  "git@github.com:bryansray/smerb.git"
-#set :branch, "stable"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :keep_releases, 5
 set :application,   'smerb'
 set :user,          'bryan'
-# set :password,      ''
 set :deploy_to,     "/var/www/#{application}"
-#set :deploy_via,    :filtered_remote_cache
-#set :repository_cache,    "/var/cache/#{application}"
-# set :monit_group,   'merb_smerb'
 
 set :production_database,'smerb_production'
 
@@ -28,3 +19,13 @@ ssh_options[:paranoid] = false
 role :app, "209.20.80.236"
 role :web, "209.20.80.236"
 role :db,  "209.20.80.236", :primary => true
+
+desc "Link in the production extras and migrate the database"
+  task :after_update_code do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+desc "Merb it up with"
+  deploy.task :restart do
+    sudo "/etc/init.d/thin restart"
+  end
