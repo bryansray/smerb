b8a561b16834e4dd1f2f3b659faa4fe058bd4a27
Bryan Ray <bryansray@gmail.com>
2008-07-25 21:48:40 -0500

    Added a display_date and display_time helper method for the DateTime class

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,11 @@ class Application < Merb::Controller
 end
 
 class DateTime
-  def display
+  def display_date
     self.strftime "%B %d, %Y"
+  end
+  
+  def display_time
+    self.strftime "%I:%M %p"
   end
 end
