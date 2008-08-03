ed6e332cc9d58184f422c27ea78e367bcb3580af
Bryan Ray <bryansray@gmail.com>
2008-08-03 02:58:53 -0500

    Added an is_spam property and a method to determine if a newly created comment is spam or human

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,6 +9,7 @@ class Comment
   property :text, Text
   property :ip, String
   property :agent, String
+  property :is_spam, Boolean, :default => true
   property :created_at, DateTime
   property :updated_at, DateTime
   
@@ -16,18 +17,13 @@ class Comment
   
   validates_present :author, :email, :text
   
-  before :save, :check_valid_comment
-  
   def to_html
     red_cloth = RedCloth.new text
     red_cloth.to_html
   end
   
-  def check_valid_comment
-    m = Mollom.new :public_key => '75595698a9ad3a5790f1dbf11d3b4369', :private_key => '13f70531c3bd4ec030786ebfb95062d6'
-    c = m.check_content :post_body => text, :author_name => author, :author_url => url, 
-                        :author_mail => email, 
-                        :author_ip => ip
-    is_spam = c.spam?
+  def is_spam=(value)
+    puts "--- #{value}"
+    attribute_set(:is_spam, false) unless value == 4
   end
 end
