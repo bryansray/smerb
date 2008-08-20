49444c36c4564af4dcf0f438e712042552d1f649
Bryan Ray <bryansray@gmail.com>
2008-08-20 09:11:20 -0500

    Hopefully this will start up google analytics again

--- public/javascripts/ga.js
+++ public/javascripts/ga.js
@@ -1,8 +1,9 @@
 <script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
 
-	var pageTracker = _gat._getTracker("UA-5104363-1");
-	pageTracker._initData();
-	pageTracker._trackPageview();
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-5104363-1");
+pageTracker._trackPageview();
 </script>
