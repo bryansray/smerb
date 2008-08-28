ed37fe1eb991e5bdceec7881b77b9c331a8d606c
Bryan Ray <bryansray@gmail.com>
2008-08-27 22:15:39 -0500

    Fixed issue with mollom.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -31,7 +31,7 @@ class Comment
   def check_comment_for_spam
       m = Mollom.new :private_key => '13f70531c3bd4ec030786ebfb95062d6', :public_key => '75595698a9ad3a5790f1dbf11d3b4369'
     
-    content = m.check_content(:post_body => text, :author_name => author, :author_url => url, :author_email => email, :author_ip => ip)
+    content = m.check_content(:post_body => text, :author_name => author, :author_url => url, :author_mail => email, :author_ip => ip)
     
     attribute_set :is_spam, content.spam? 
   end
